3e0441b730686b6e165240d1468f14ea2e3b6e82
Erickson Joseph <erickson1.joseph@gmail.com>
2016-12-02 21:27:21 -0500

    nil channels

---
+++ nil-channels/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	var isClosed1, isClosed2 bool
+
+	exit := make(chan bool)
+	ch1 := make(chan bool)
+	ch2 := make(chan bool)
+
+	// Close channel 1 after 3 seconds
+	time.AfterFunc(3*time.Second, func() {
+		fmt.Println("closing 1")
+		close(ch1)
+		isClosed1 = true
+	})
+
+	// Close channel 2 after 6 seconds
+	time.AfterFunc(6*time.Second, func() {
+		fmt.Println("closing 2")
+		close(ch2)
+		isClosed2 = true
+	})
+
+	// Exit after 8 seconds
+	time.AfterFunc(8*time.Second, func() {
+		fmt.Println("time")
+		exit <- true
+	})
+
+	for !isClosed1 || !isClosed2 {
+
+		fmt.Println(".")
+
+		// A closed channel will always be selected
+		// So we need to make the channel nil to be ignored
+		select {
+		case i, open := <-ch1:
+			fmt.Println("ch1 received. i:", i, "open:", open)
+			if !open {
+				// If you comment the following line, you'll see that the
+				// second case may never be called
+				ch1 = nil
+			}
+		case i, open := <-ch2:
+			fmt.Println("ch2 received. i:", i, "open:", open)
+			if !open {
+				ch2 = nil
+			}
+		}
+	}
+
+	fmt.Println("loop over")
+
+	var a bool
+
+	a = <-exit
+
+	fmt.Println("exited", a)
+}
